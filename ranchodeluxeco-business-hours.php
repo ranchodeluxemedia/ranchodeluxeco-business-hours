6e330e9d6f1704fc1c85e9050fa2359257504830
Chris Canterbury <ranchodeluxeco@Chriss-MacBook-Pro.local>
2019-03-29 15:43:30 -0500

    Initial commit

---
+++ ranchodeluxeco-business-hours.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Ranchodeluxeco Business Hours
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     ranchodeluxeco-business-hours
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ranchodeluxeco_Business_Hours
+ */
+
+// Your code starts here.
